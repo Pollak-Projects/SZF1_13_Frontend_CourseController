75d6d57a129d8f3d3e590b6f54c6ef272ea1a30d
IWillSettYou <bmate20050911@gmail.com>
2024-10-08 13:03:33 +0200

    Started working on the navigation and defined the section component that should supposedly contain the site's content

--- CourseController/src/App.vue
+++ CourseController/src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-
+import Navigation from "./components/Navigation.vue";
 </script>
 
 <template>
-<div class="bg-red-100">asd</div>
+  <div class="flex items-center h-[100dvh]">
+
+    <section class=" h-[96dvh] w-1/6 bg-primary  mx-[2dvh] p-[1dvh] rounded-lg">
+      <Navigation/>
+    </section>
+    <section class="h-[96dvh] w-5/6 bg-primary  mx-[2dvh] p-[1dvh] rounded-lg">
+
+    </section>
+  </div>
+
+
 </template>
 
 <style scoped>
