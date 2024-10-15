d139ab83bd1c36edd98aacb5eb32d6d953e147dd
TGyAkos <t.gyula.akos@gmail.com>
2024-10-15 09:12:15 +0200

    added admin path, everything works

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,7 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 
 const routes = [
     { path: '/', component: () => import("./components/HelloWorld.vue") },
+    { path: '/admin', component: () => import("./views/admin.vue") },
 ]
 
 const router = createRouter({
