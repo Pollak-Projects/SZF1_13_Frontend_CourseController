3659c482435e7a173ada0f53976d141e55751cb1
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-10-15 19:38:56 +0200

    started to add the Admin page

--- CourseController/src/components/AdminNavbar.vue
+++ CourseController/src/components/AdminNavbar.vue
@@ -3,7 +3,8 @@
 </script>
 
 <template>
-<div>
+<!--  TODO add actual buttons with pages behind them-->
+<div class="flex py-[0.5dvh] rounded-b-3xl shadow-figmaxl justify-evenly">
   <p>Modulok</p>
   <p>Feladatok</p>
   <p>Felhasznalok</p>
---
+++ CourseController/src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+<!--  TODO this is where the dashboard may go in the future-->
+<p>hi</p>
+</template>
+
+<style scoped>
+
+</style>
