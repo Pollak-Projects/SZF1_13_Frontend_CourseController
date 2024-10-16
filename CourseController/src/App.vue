4bcdea0c04cfd0c231645b43d9a3bfe9b2104703
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-10-16 22:25:14 +0200

    removed debug colors form App.vue

--- CourseController/src/App.vue
+++ CourseController/src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
 import UserIcon from "./components/UserIcon.vue";
-
 </script>
 
 <template>
-<!--  TODO remove circus ahh looking background colors (these are for debug visualisations) -->
-  <div class="grid grid-cols-[25dvw_50dvw_25dvw] grid-rows-none gap-0 h-[100dvh] w-[100dvh] bg-gradient-to-b from-red-400 to-orange-400">
-    <div class="bg-green-500">
+<!--  This background isn't in the design its just a placeholder for now -->
+  <div class="grid grid-cols-[25dvw_50dvw_25dvw] grid-rows-none gap-0 h-[100dvh] w-[100dvw] bg-gradient-to-b from-red-400 to-orange-400">
+    <div class="">
       <div id="rightCol"></div>
     </div>
-    <main class="z-10 bg-yellow-500">
+    <main class="z-10">
       <RouterView />
     </main>
-    <div class="flex justify-end bg-violet-500">
+    <div class="flex justify-end ">
       <div class="m-[2dvh_2dvw] h-fit ">
         <UserIcon />
       </div>
