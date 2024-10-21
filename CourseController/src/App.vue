dcb07633e7c819cbb260382e36f6848b5cafa0a7
Imii <76595709+iambestaim@users.noreply.github.com>
2024-10-21 10:29:41 +0200

    Add files via upload

--- CourseController/src/App.vue
+++ CourseController/src/App.vue
@@ -1,27 +1,41 @@
 <script setup>
 import UserIcon from "./components/UserIcon.vue";
+import Feladatsor from "./components/Feladatsor.vue";
+import View from "./components/View.vue";
+
 
 </script>
 
 <template>
 <!--  TODO remove circus ahh looking background colors (these are for debug visualisations) -->
-  <div class="grid grid-cols-[25dvw_50dvw_25dvw] grid-rows-none gap-0 h-[100dvh] w-[100dvh] bg-gradient-to-b from-red-400 to-orange-400">
-    <div class="bg-green-500">
-      <div id="rightCol"></div>
-    </div>
-    <main class="z-10 bg-yellow-500">
-      <RouterView />
-    </main>
-    <div class="flex justify-end bg-violet-500">
-      <div class="m-[2dvh_2dvw] h-fit ">
-        <UserIcon />
+<div class="niger">
+  <div class="grid grid-cols-[25dvw_75dvw] grid-rows-none gap-0 h-[100dvh] max-h-[100dvh] w-[130dvh] bg-gradient-to-b">
+
+    <div class="">
+      <div id="rightCol">
+
+        <Feladatsor/>
+
       </div>
     </div>
+      <View/>
+      <RouterView />
+
+    </div>
   </div>
-
-
 </template>
 
 <style scoped>
+.niger {
+    --_mesh-gradient-blur: 90px;
+    --_mesh-gradient-blend-mode: normal;
 
+    background: 
+        radial-gradient(circle at top left, #000000 0%, rgba(0, 0, 0, 0) 100%),
+        radial-gradient(circle at center bottom, #cb0fd1 0%, rgba(161, 92, 193, 0) 100%),
+        radial-gradient(circle at top right, #27b2e0 0%, rgba(0, 191, 255, 0) 100%);
+        
+    mix-blend-mode: var(--_mesh-gradient-blend-mode);
+    background-size: cover; /* A háttérképek kitöltése */
+}
 </style>
