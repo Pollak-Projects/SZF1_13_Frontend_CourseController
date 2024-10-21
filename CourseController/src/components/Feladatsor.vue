63f6526f0f382bd9ced32785eec0a849e10d21b8
Imii <76595709+iambestaim@users.noreply.github.com>
2024-10-21 10:29:06 +0200

    + Changes

---
+++ CourseController/src/components/Feladatsor.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="">
+    <div class="mt-28 border-1 rounded-2xl border-black mr-10 bg-black bg-opacity-10" style="box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5), 0 -4px 0 rgba(0, 0, 0, 0);">
+      <h1 class="text-center text-gray-500 text-3xl p-4">HTML Alapok</h1>
+      <h2 class="text-3xl text-pink-400">Big Tananyag</h2>
+      <p>dkhsafokjsdbhrsudigshiuhgkuhiughuig</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ CourseController/src/components/View.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class=" justify-center items-center max-h-screen ">
+    <div class="mt-28 border-1 rounded-2xl border-black ml-32 bg-black bg-opacity-10 mr-52" style="box-shadow: 0 4px 10px rgba(0, 0, 0, 0.5), 0 -4px 0 rgba(0, 0, 0, 0);">
+      <h1 class="text-center text-gray-500 text-3xl p-4">HTML Alapok</h1>
+      <h2 class="text-3xl text-pink-400">Big Tananyag</h2>
+      <p>dkhsafokjsdbhrsudigshiuhgkuhiughuig</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h2{
+    padding-bottom: 1.2dvw;
+    padding-left: 1.7dvw;
+    font-weight: 500;
+}
+
+
+p {
+  padding-left: 1.7dvw;
+  padding-bottom: 10px;
+}
+</style>
