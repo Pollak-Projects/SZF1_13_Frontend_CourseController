886cf47ac2b3dc928043f2804172926d7e03e379
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-10-28 07:36:11 +0100

    resolved router conflict

--- CourseController/src/router.js
+++ CourseController/src/router.js
@@ -1,15 +1,14 @@
-import { createMemoryHistory, createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 // TODO someday replace this with an auto generated router
 // pain in the butt to batch add new routes
 const routes = [
-<<<<<<< HEAD
+
     //{ path: '/', component: () => import("./views/Home.vue") },
     { path: '/', component: () => import("./views/LandingPage.vue") },
-    { path: '/admin', component: () => import("./views/Admin.vue") },
     { path: '/landing', component: () => import("./views/LandingPage.vue") },
     { path: '/:pathMatch(.*)*', name: 'NotFound', component: () => import("./views/NotFound.vue") },
-=======
+
     { path: '/', component: () => import("./views/Home.vue")},
     {
         path: '/admin',
@@ -29,7 +28,6 @@ const routes = [
             component: () => import("./views/admin/AdminUsers.vue"),
         }]
     },
->>>>>>> e74891d (Navigation now works in the admin page and sub-pages)
 ]
 
 const router = createRouter({
