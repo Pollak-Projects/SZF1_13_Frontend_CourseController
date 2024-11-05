56d3391169303e47de902c474c00b7789d89ada1
Barta Máté <114726553+IWillSettYou@users.noreply.github.com>
2024-11-05 11:09:01 +0100

    Update Admin.vue

--- CourseController/src/views/Admin.vue
+++ CourseController/src/views/Admin.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import {provide, ref} from "vue";
+import AdminModules from "./admin/AdminModules.vue";
+import AdminAssignments from "./admin/AdminAssignments.vue";
+import AdminUsers from "./admin/AdminUsers.vue";
 import BaseLayout from "../slots/BaseLayout.vue";
 
-const isAdmin = ref(false)
 
-provide('isAdmin', isAdmin)
+const isAdmin = ref(false)
 
 const toggleAdmin = () => {
   isAdmin.value = !isAdmin.value
@@ -24,11 +26,13 @@ const toggleAdmin = () => {
         </div>
     </template>
     <template #main>
-      <router-view/>
-
+      <router-view :is-admin="isAdmin" />
     </template>
     <template #right>
-      <UserIcon/>
+      <div class="flex flex-col gap-4 items-end">
+        <UserIcon/>
+        <Button label="Switch admin" icon="pi pi-cog" @click="toggleAdmin" />
+      </div>
     </template>
   </BaseLayout>
 
