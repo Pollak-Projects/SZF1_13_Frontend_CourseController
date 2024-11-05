07cc624b87ead3a8086cc93be25250e78fd49812
Barta Máté <114726553+IWillSettYou@users.noreply.github.com>
2024-11-05 11:08:30 +0100

    Update AdminUsers.vue

--- CourseController/src/views/admin/AdminUsers.vue
+++ CourseController/src/views/admin/AdminUsers.vue
@@ -1,23 +1,19 @@
 <script setup lang="ts">
 import BaseLayout from "../../slots/BaseLayout.vue";
 import UserIcon from "../../components/UserIcon.vue";
+import AdminModule from "../../components/AdminModule.vue";
+import AdminNavbar from "../../components/AdminNavbar.vue";
 
-
+const props = defineProps({
+  isAdmin: Boolean,
+})
 </script>
 <template>
-  <BaseLayout>
-    <template #left>
-      <Teleport to="#rightCol">
-        <div class="m-[2dvh_2dvw]">
-          <RouterLink to="/admin">
-            <Button icon="pi pi-arrow-left" aria-label="Back" />
-          </RouterLink>
-        </div>
-      </Teleport>
-    </template>
-    <template #main></template>
-    <template #right>
-      <UserIcon/>
-    </template>
-  </BaseLayout>
+
+  <AdminNavbar :is-admin="isAdmin" />
+
+  <div class="m-[2dvh_2dvw]">
+
+  </div>
+
 </template>
