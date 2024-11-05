e29eaf57efa189205e275d478c82cbaa4aefa3e9
Barta Máté <114726553+IWillSettYou@users.noreply.github.com>
2024-11-05 11:06:57 +0100

    Update AdminAssignments.vue

--- CourseController/src/views/admin/AdminAssignments.vue
+++ CourseController/src/views/admin/AdminAssignments.vue
@@ -1,23 +1,15 @@
 <script setup>
 
-import BaseLayout from "../../slots/BaseLayout.vue";
-import UserIcon from "../../components/UserIcon.vue";
 
+
+import AdminNavbar from "../../components/AdminNavbar.vue";
+
+const props = defineProps({
+  isAdmin: Boolean,
+})
 </script>
 <template>
-  <BaseLayout>
-    <template #left>
-      <Teleport to="#rightCol">
-        <div class="m-[2dvh_2dvw]">
-          <RouterLink to="/admin">
-            <Button icon="pi pi-arrow-left" aria-label="Back" />
-          </RouterLink>
-        </div>
-      </Teleport>
-    </template>
-    <template #main></template>
-    <template #right>
-      <UserIcon/>
-    </template>
-  </BaseLayout>
+
+      <AdminNavbar :is-admin="isAdmin" />
+
 </template>
