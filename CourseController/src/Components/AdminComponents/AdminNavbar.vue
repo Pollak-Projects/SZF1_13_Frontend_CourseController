26d1a60d3dc6af0ebb98eb36049749adc8c34820
TGyAkos <t.gyula.akos@gmail.com>
2024-11-12 12:24:12 +0100

    started adding carets to dropdowns

--- CourseController/src/Components/AdminComponents/AdminNavbar.vue
+++ CourseController/src/Components/AdminComponents/AdminNavbar.vue
@@ -18,7 +18,10 @@ const showUsersDropdown = ref(false);
           @mouseenter="showModulesDropdown = true"
           @mouseleave="showModulesDropdown = false"
       >
-        <RouterLink to="/admin/modules" class="px-4 py-2 hover:bg-purple-800 rounded">Modulok</RouterLink>
+        <RouterLink to="/admin/modules" class="px-4 py-2 hover:bg-purple-800 rounded">Modulok
+          <i v-if="showModulesDropdown"  class="pi pi-angle-up text-base"/>
+          <i v-else class="pi pi-angle-down text-base"/>
+        </RouterLink>
         <div
             v-if="showModulesDropdown"
             class="absolute top-full left-0 mt-1 w-32 bg-white text-gray-800 shadow-lg rounded-lg"
@@ -34,7 +37,10 @@ const showUsersDropdown = ref(false);
           @mouseenter="showAssignmentsDropdown = true"
           @mouseleave="showAssignmentsDropdown = false"
       >
-        <RouterLink to="/admin/assignments" class="px-4 py-2 hover:bg-purple-800 rounded">Feladatok</RouterLink>
+        <RouterLink to="/admin/assignments" class="px-4 py-2 hover:bg-purple-800 rounded">Feladatok
+          <i v-if="showAssignmentsDropdown" class="pi pi-angle-up text-base"/>
+          <i v-else class="pi pi-angle-down text-base"/>
+        </RouterLink>
         <div
             v-if="showAssignmentsDropdown"
             class="absolute top-full left-0 mt-1 w-32 bg-white text-gray-800 shadow-lg rounded-lg"
@@ -51,7 +57,10 @@ const showUsersDropdown = ref(false);
           @mouseenter="showUsersDropdown = true"
           @mouseleave="showUsersDropdown = false"
       >
-        <RouterLink to="/admin/users" class="px-4 py-2 hover:bg-purple-800   rounded">Felhasználók</RouterLink>
+        <RouterLink to="/admin/users" class="px-4 py-2 hover:bg-purple-800 rounded">Felhasználók
+          <i v-if="showUsersDropdown" class="pi pi-angle-up text-base"/>
+          <i v-else class="pi pi-angle-down text-base"/>
+        </RouterLink>
         <div
             v-if="showUsersDropdown"
             class="absolute top-full left-0 mt-1 w-32 bg-white text-gray-800 shadow-lg rounded-lg"
