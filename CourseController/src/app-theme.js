74e270bb5c0fed8338bd2a419be430f053dc78bc
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-11-15 18:58:24 +0100

    finally got primevue working

---
+++ CourseController/src/app-theme.js
@@ -0,0 +1,27 @@
+import {definePreset} from "@primevue/themes";
+import Aura from "@primevue/themes/aura";
+
+export const CourseControlPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: 'rgba(10, 0, 0, 0.5)',
+            100: 'rgba(10, 0, 0, 0.5)',
+            200: 'rgba(10, 0, 0, 0.5)',
+            300: 'rgba(10, 0, 0, 0.5)',
+            400: 'rgba(10, 0, 0, 0.5)',
+            500: 'rgba(10, 0, 0, 0.5)',
+            600: 'rgba(10, 0, 0, 0.5)',
+            700: 'rgba(10, 0, 0, 0.5)',
+            800: 'rgba(10, 0, 0, 0.5)',
+            900: 'rgba(10, 0, 0, 0.5)',
+            950: 'rgba(10, 0, 0, 0.5)',
+        },
+        colorScheme: {
+            light: {
+                primary: {
+                    contrastColor: "#ffffff",
+                }
+            },
+        },
+    },
+})
